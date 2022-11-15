ce90ebeca99f5cbea7500f64e8ba42a808b69a19
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-15 13:11:52 +0100

    Update CartItem.js

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -19,3 +19,47 @@ const CartItem = ({cartItem})=> {
 
 export default CartItem;
 
+/*
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import dataDesserts from '../../data/dataDesserts';
+import {  removeItemFromCart, getCartItems, updateQuantity, addItemToCart } from "../../redux/cartSlice";
+import ChangeQuantity from './ChangeQuantity';
+
+const CartItem = ({cartItem})=> {
+   const [quantity, setQuantity] = useState(1);
+    const desserts= dataDesserts.find(item=> item.id === cartItem.dessertId)
+    const dispatch= useDispatch();
+
+    const items = useSelector(getCartItems);
+    const itemsInCart = items.some(item => item.dessertId === item.id);
+    
+    const addToCart = () => {
+    if (!itemsInCart) {
+        dispatch(addItemToCart({cartItem, quantity}));
+        setQuantity(1)
+    } else {
+        dispatch(updateQuantity({cartItem, quantity}))
+        setQuantity(1)
+    }
+}
+
+    return(
+    <div>
+    <div>
+        <img src={`./${desserts.img}.jpg`} alt="cart item" width="100px"/>
+       <p>{desserts.name}</p>
+       <p>{cartItem.quantity}portion(s) ${desserts.price * cartItem.quantity }   <span onClick={()=>dispatch(removeItemFromCart({cartItemId: cartItem.id}))}>
+       <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt='delete'/> 
+       </span>
+         </p>  
+    </div>
+    <ChangeQuantity quantity={quantity} setQuantity={setQuantity}/>
+     {/* <button onClick={addToCart}>click</button>  */}
+    </div>
+    
+    )
+}
+
+export default CartItem;
+*/
